ace142b9c479b7ebc6bd4a200cb9a60f7c0f1bcc
David Hartman <david.j.hartman@gmail.com>
2025-02-08 09:01:19 -0500

    Updated function header comment block and class header block

--- sample/core.py
+++ sample/core.py
@@ -10,16 +10,42 @@ Any imports/dependencies/requirements should be noted here (e.g. argparse, etc)
 # import any functions inside or outside of this module.  If no helpers are needed it can be removed from here
 # as well as remove the file from the sample module.
 
+__version__ = '2021.01'
+__author__ = 'David Hartman'
+
+import sys
+import os
+import datetime
+
+# The below is how variables should be documented with docstrings
+
+#: This effectively defines the root of the project and so adding ..\, etc is not needed in config files
+PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+
+#: Directory that contains configuration files, update as needed.
+CONF_DIR = os.path.join(PROJECT_ROOT_DIR, 'conf')
+
+#: Directory were data files/extracts/reports will be stored
+DATA_DIR = os.path.join(PROJECT_ROOT_DIR, 'data')
+
+FILENAME_INPUT_CONFIG = os.environ.get('CONFIG_FILE_PATH',
+                                       os.path.join(CONF_DIR, 'test.conf'))
+
+
 from . import helpers
 
+
 def get_hmm():
-    """This is a place holder for an internal function to the module.  it should be removed/replaced as needed.
+	"""This is a place holder for an internal function to the module.  it should be removed/replaced as needed.
 
-    """
-    return 'hmmm...'
+	"""
+	return 'hmmm...'
 
 
-def hmm():
-    """This is a that can be called outside this module by way of an import."""
-    if helpers.get_answer():
-        print(get_hmm())
+def function_example(param1, param2withdefault=False):
+	"""This is a that can be called outside this module by way of an import."""
+	if helpers.get_answer():
+		print(get_hmm())
+
+if __name__ == "__main__":
+	function_example()
